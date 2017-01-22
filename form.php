73264bbd5fa9fae9a95b9798c5502fc78b4e0ab9
j0ly <johannes.lyttbacka@centria.fi>
2017-01-22 13:24:53 +0200

    Update form.php

--- form.php
+++ form.php
@@ -11,7 +11,7 @@
         <form action="done.php" method="POST">
             <label for="name">Name</label>: <input type="text" name="name" id="name"/><br>
             <label for="address">Address</label>: <input type="text" name="address" id="address"/><br><br>
-            <label for="creditcard">Credit Card number</label>: <input type="text" name="creditcard" id="creditcard"/> (don't enter real credit card numbers here)<br>   
+            <label for="creditcard">Credit Card number</label>: <input type="text" name="creditcard" id="creditcard"/> (don't enter real credit card numbers)<br>   
             <p><input type="submit" value="Submit" /></p>
         </form>
 
